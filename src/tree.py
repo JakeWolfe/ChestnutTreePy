e5459be0c2606e85e395eb78d00ac930d4459c55
Quentin Goehrig <goehrig.quentin@uwlax.edu>
2018-10-21 18:15:12 -0500

    added getters, setters to tree

--- src/tree.py
+++ src/tree.py
@@ -9,9 +9,10 @@ rating (health), stage (size), and treatment.
 """
 
 class Tree:
+    
     # Jake, I'm leaving the constructor to take in a x, y position as opposed
     # to a Point class for simplicity.
-    def __init__(self, x, y, rating, stage, treatment):
+    def __init__(self, x = 0, y = 0, rating = 0, stage = 0, treatment = 0):
         self._x = x
         self._y = y
         self._rating = rating # health
@@ -20,24 +21,59 @@ class Tree:
         
     #def __eq__(self, other):
         # May need to define custom '==' operator or .equals function
-    
-    @property
-    def x(self):
-        """ Gets the '_x' property """
-        return self._x
-    
-    @property
-    def x.setter(self, x):
-        """ Sets the '_x' property """
-        return self._x = x
         
     def print_tree(self):
         print( "Tree (" + str(self.x) + ", " + str(self.y) + "):\n  Rating: " +
               str(self.rating) + "\n  Stage: " + str(self.stage) +
               "\n  Treatment: " + str(self.treatment) )
     
-     
-test1 = Tree(1,1,2,3,4);
-test2 = Tree(1,1,2,3,4);
+    # Property getters, setters. Jake, let me know if I'm being too verbose
+    @property
+    def x(self):
+        return self._x
+    
+    @x.setter
+    def x(self, val):
+        self._x = val
+        
+    @property
+    def y(self):
+        return self._y
+    
+    @y.setter
+    def y(self, val):
+        self._y = val
+        
+    @property
+    def rating(self):
+        return self._rating
+    
+    @rating.setter
+    def rating(self, val):
+        self._rating = val
+        
+    @property
+    def stage(self):
+        return self._stage
+    
+    @stage.setter
+    def stage(self, val):
+        self._stage = val
+        
+    @property
+    def treatment(self):
+        return self._stage
+    
+    @treatment.setter
+    def treatment(self, val):
+        self._treatment = val
+    
+    
+# Tests
+tree = Tree(0);
 
-test1.print_tree()
+tree.x = 1
+tree.y = 2
+tree.rating = 3
+
+tree.print_tree()
